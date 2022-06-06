51865730262b89071cce48d32b1981471dbdd3c7
under4 <uonder50@gmail.com>
2022-06-07 00:05:22 +0300

    initial

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+try{
+    $db = new PDO('mysql:host=localhost;dbname=blog;charset=UTF8','root','');
+} catch(PDOException $error) {
+    echo "couldnt connect to database";
+    echo "<br>";
+    die($error);
+}
+?>
---
+++ header.php
@@ -0,0 +1,34 @@
+<?php 
+session_start();
+require_once("db.php");
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>  
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/root.css">
+        <link rel="stylesheet" href="css/header.css">
+    </head>
+    
+
+    <body>
+        <div>
+            <div id="logo">
+                The BLOG
+            </div>
+            <div id="links">
+                <a href="#">Pholder</a>
+                <a href="#">Pholder</a>
+                <a href="#">Pholder</a>
+                <a href="#">Pholder</a>
+            </div>
+        </div>
+        <div id="login">
+            <a href="#">Login</a>
+            <a href="#">Sign Up</a>
+        </div>
+    </body>
+</html>
