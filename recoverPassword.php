3f977f5bf66c02339fe7ecbcee0e9cbb47a25702
under4 <uonder50@gmail.com>
2022-06-13 23:12:56 +0300

    recover

---
+++ recoverPassword.php
@@ -0,0 +1,21 @@
+<?php 
+require_once("header.php")
+?>
+<!DOCTYPE html>
+<html>
+    <head>  
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://kit.fontawesome.com/19a4fd8ab0.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="css/root.css">
+        <link rel="stylesheet" href="css/recover.css">
+    </head>
+    <body>
+        <div class="flex">
+            <div id="main">
+
+            </div>
+        </div>
+    </body>
+</html>
